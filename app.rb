3c45242475518672dbc6d9e7d2a20992466d58da
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-20 20:21:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/hello' do
+		erb :hello
 	end
+
+	get '/date' do
+		erb :date
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	# get '/' do
+	# 	erb :index
+	# end
 
 
 end
