6cb1abca1beac90910ec9bf74ebc344d8ec5a74b
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-20 20:04:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 class App < Sinatra::Base
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/date' do
+		erb :date
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
 	get '/' do
 		erb :index
 	end
